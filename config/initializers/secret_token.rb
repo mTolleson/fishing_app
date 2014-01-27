d54f1aabc643f1ef52b3fff37839a32910e6fa46
Matt Tolleson <mtolleson@icloud.com>
2014-01-27 12:30:52 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FishingApp::Application.config.secret_key_base = '4c6b5119f00b1f8d9db2ca205d3f0d3222c2c5180fdd6c5f7b5521ddb9fa1894ac653ad0ee54fbe7024234b951f36dbae876021893b26159f171938b8b7e992d'
